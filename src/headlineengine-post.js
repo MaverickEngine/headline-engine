1248899985a8747563ea5c48fb60cf2b6f81b986
Jason Norwood-Young <jason@10layer.com>
2022-06-24 11:56:44 +0200

    Initial working system

---
+++ src/headlineengine-post.js
@@ -0,0 +1,83 @@
+require("./headlineengine-post-style.scss");
+require("wordsmith-js/dist/wordsmith.min.js");
+const Powerwords = require("./powerwords");
+console.log(Wordsmith.fleschReadingEaseScore("Quadraphonia is an extremely long phrase."));
+const readable_range = [45, 90];
+const readable_range_min = readable_range[0];
+const readable_range_max = readable_range[1];
+const readability_messages = ["Good", "Too Simple", "Too Complex"];
+
+const length_range = [40, 90];
+const length_messages = ["Good", "Too Short", "Too Long"];
+
+function readability(title) {
+    if (!title) return;
+    const score = Wordsmith.fleschReadingEaseScore(title);
+    let rating = 0;
+    if (score < readable_range_min) {
+        rating = 2;
+    } else if (score > readable_range_max) {
+        rating = 1;
+    }
+    return {rating, score, message: readability_messages[rating], pass: score >= readable_range_min && score <= readable_range_max };
+}
+
+function length(title) {
+    if (!title) return;
+    const length = title.length;
+    let rating = 0;
+    if (length < length_range[0]) {
+        rating = 1;
+    } else if (length > length_range[1]) {
+        rating = 2;
+    }
+    return { rating, length, message: length_messages[rating], pass: length >= length_range[0] && length <= length_range[1] };
+}
+
+function powerwords(title) {
+    if (!title) return;
+    title = title.toLowerCase().replace(/[^a-z]/g, " ");
+    const words = title.split(" ").filter(w => w.length > 3);
+    const powerwords_found = words.filter(w => Powerwords.includes(w));
+    return { rating: powerwords_found.length, words: powerwords_found, pass: powerwords_found.length > 0 };
+}
+
+function display_analysis() {
+    const title = jQuery("#title").val();
+    if (!title) {
+        jQuery("#headlineengine-score-container").html("");
+        return;
+    }
+    const length_result = length(title);
+    const readability_result = readability(title);
+    const powerwords_result = powerwords(title);
+    const score = length_result.pass + readability_result.pass + powerwords_result.pass;
+    const rating = score >= 3 ? "good" : score >= 1 ? "okay" : "bad";
+    const score_el = jQuery(`
+    <div class='headlineengine-score'>
+        <div class='headlineengine-score-value headlineengine-score-value-${rating}'>${ Math.floor(score / 3 * 100) }</div>
+        <div class='headlineengine-score-title'>Headline Score</div>
+    </div>`);
+    const analysis = jQuery(`<div class="headlineengine-analysis">
+        <div class="headlineengine-analysis-readability">Readability: ${readability_result.message} (${Math.round(readability_result.score)})</div>
+        <div class="headlineengine-analysis-length">Length: ${length_result.message} (${length_result.length})</div>
+        <div class="headlineengine-analysis-powerwords">Powerwords: ${(powerwords_result.words.length) ? powerwords_result.words.map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(", ") : "None" }</div>
+    </div>`);
+    jQuery("#headlineengine-score-container").html(score_el);
+    jQuery("#headlineengine-score-container").append(analysis);
+
+}
+
+async function main() {
+    jQuery(() => {
+        // const titlewrap_el = jQuery("#titlewrap");
+        // const ab_el = jQuery("<input type='button' class='button' value='A/B' id='headlineengine-main-title-ab' />");
+        // titlewrap_el.append(ab_el);
+        display_analysis();
+    });
+    jQuery("#title").on("keyup", function(e) {
+        display_analysis();
+    })
+}
+
+main();
