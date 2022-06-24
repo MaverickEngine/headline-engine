08cda9753fd88345202ed2c0aa16f6d9a8fb2d9f
Jason Norwood-Young <jason@10layer.com>
2022-06-24 14:40:27 +0200

    Don't log

--- src/headlineengine-admin.js
+++ src/headlineengine-admin.js
@@ -1,5 +1,4 @@
 jQuery("#headlineengine_load_powerwords").click(async function() {
     const powerword_list = await (await fetch(headlineengine_powerwords_url)).text();
-    console.log(powerword_list);
     jQuery("#headlineengine_powerwords_list").val(powerword_list);
 });
